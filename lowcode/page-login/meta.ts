34ddbb26a663a808bda76222482aa49e99790ece
cxy <chenxiaoyi27@163.com>
2023-09-20 14:11:33 +0800

    查询页bug

--- lowcode/page-login/meta.ts
+++ lowcode/page-login/meta.ts
@@ -11,7 +11,7 @@ const PageLoginMeta: IPublicTypeComponentMetadata = {
   "devMode": "proCode",
   "npm": {
     "package": "material-page-cxy",
-    "version": "0.1.1",
+    "version": "0.1.2",
     "exportName": "PageLogin",
     "main": "src\\index.tsx",
     "destructuring": true,
